9a0518c60332a724875159d29e13e094fa05d685
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-03 00:09:27 +0800

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,18 +6,18 @@ use PHPUnit\Framework\TestCase;
 
 use App\user_dashboard\index;
 
-// ** @test **
+
 class UserTest extends TestCase {
-    
+
         // User Testing
-    public function Usertest(){
-        $x = True;
-        if ($x == True){
-            echo ' User Test 1: Test Passed ';
-            echo '';
+    public function testOne(){
+        $y = True;
+        if ($y == True){
+            echo ' User Test 2: Test Passed ';
+            echo '<br>';
         } else{
-            echo ' User Test 1: Test Failed ';
-            echo '';
+            echo ' User Test 2: Test Failed ';
+            echo '<br>';
         }      
     }
 }
