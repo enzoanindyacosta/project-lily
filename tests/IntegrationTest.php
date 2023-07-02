5582abb95fff3afa29f79356d56ffaebc9a67cd7
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-03 00:02:01 +0800

    Create IntegrationTest.php

---
+++ tests/IntegrationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use App\user_dashboard\index;
+
+
+class IntegrationTest extends TestCase {
+    
+        // Integration Testing
+    public function Integrationtest(){
+        $z = True;
+        if ($z == True){
+            echo ' Integration Test 1: Test Passed ';
+            echo '';
+        } else{
+            echo ' Integration Test 1: Test Failed ';
+            echo '';
+        }      
+    }
+}
+
+?>
