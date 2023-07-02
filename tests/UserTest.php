d845a5d50fd4ca4c553789e64209e3d422a27002
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-02 23:46:47 +0800

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -8,31 +8,35 @@ use App\user_dashboard\index;
 
 /** @test */
 class UserTest extends TestCase {
-    public function testUserSidebar(){
-        $x = True;
-        
+    
         // User Testing
+    public function testOne(){
+        $x = True;
         if ($x == True){
             return 'User Test 1: Test Passed';
         } else{
             return 'User Test 2: Test Failed';
-        }
+        }      
+    }
+    
         // Unit Testing
+    public function testTwo(){
+        $y = True;
         if ($x == True){
             return 'Unit Test 1: Test Passed';
         } else{
             return 'Unit Test 2: Test Failed';
-        }
+        }      
+    }
+    
         // Integration Testing
+    public function testThree(){
+        $z = True;
         if ($x == True){
             return 'Integration Test 1: Test Passed';
         } else{
             return 'Integration Test 2: Test Failed';
-        }
-
-
-        
-        
+        }       
     }
 }
   
