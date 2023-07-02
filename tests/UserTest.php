2cd72068b02dca7304b4b0afb1b8dafbea2ed175
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-02 23:29:01 +0800

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 use App\user_dashboard\index;
 
 /** @test */
-class user_sidebar_test extends TestCase {
+class UserTest extends TestCase {
     public function testUserSidebar(){
         echo'Test Passed';
     }
