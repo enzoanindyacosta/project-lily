c54e493cc494c8cb301c33064073a078ac60c158
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-02 23:58:13 +0800

    Create UnitTest.php

---
+++ tests/UnitTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use App\user_dashboard\index;
+
+
+class UnitTest extends TestCase {
+  
+        // Unit Testing
+    public function testTwo(){
+        $y = True;
+        if ($y == True){
+            echo ' Unit Test 2: Test Passed ';
+            echo '';
+        } else{
+            echo ' Unit Test 2: Test Failed ';
+            echo '';
+        }      
+    }
+}
+  
+?>
