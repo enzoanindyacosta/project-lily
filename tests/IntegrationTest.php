9f71b67a9b41ee11357c4d8ccc2048b395e4b0bb
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-03 00:10:28 +0800

    Update IntegrationTest.php

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -8,16 +8,16 @@ use App\user_dashboard\index;
 
 
 class IntegrationTest extends TestCase {
-    
+
         // Integration Testing
-    public function Integrationtest(){
+    public function testTwo(){
         $z = True;
         if ($z == True){
-            echo ' Integration Test 1: Test Passed ';
-            echo '';
+            echo ' Integration Test 2: Test Passed ';
+            echo '<br>';
         } else{
-            echo ' Integration Test 1: Test Failed ';
-            echo '';
+            echo ' Integration Test 2: Test Failed ';
+            echo '<br>';
         }      
     }
 }
