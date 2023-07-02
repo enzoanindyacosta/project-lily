fec0a0682c015881e80e7a57aed181f943b5572e
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-02 23:03:18 +0800

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,21 +7,9 @@ use PHPUnit\Framework\TestCase;
 use App\components\user_sidebar;
 
 class user_sidebar_test extends TestCase {
-
     public function testUserSidebar(){
-        print_r(get_included_files());
+        print_r(get_included_files()
+    };
+};
   
-}
-  
-
-
-
-
-    
-// final class UserTest extends TestCase
-// {
-//     // Tests will go here
-// }
-
-
-  ?>
+?>
