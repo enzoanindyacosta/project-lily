9188b56996aeb37f868a3066b63daf871e6bec3e
Enzo Anindya Costa <119120874+enzoanindyacosta@users.noreply.github.com>
2023-07-02 23:20:15 +0800

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,10 +7,9 @@ use PHPUnit\Framework\TestCase;
 use App\user_dashboard\index;
 
 class user_sidebar_test extends TestCase {
-    echo'Test Passed'
-    // public function testUserSidebar(){
-    //     print_r(get_included_files())
-    // }
+    public function testUserSidebar(){
+        echo'Test Passed';
+    }
 }
   
 ?>
